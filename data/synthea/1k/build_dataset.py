c14d3c71f384c5685ba777129d14b704f1c40161
amirhkr <amirhossein.kamel@gmail.com>
2025-04-12 13:10:47 +1000

    exclude drugs with concept id 0

--- data/synthea/1k/build_dataset.py
+++ data/synthea/1k/build_dataset.py
@@ -71,7 +71,7 @@ else:
         else:
             hadms_with_empty_diagnoses.append(hadm_id)
         
-        if drug_code:
+        if drug_code and drug_code != '0':
             subject_visits[-1].append(f'{drug_code}')
         else:
             hadms_with_empty_drugs.append(hadm_id)
